da4bbd4ee98cfdee012aff2902bcd5d6fad1b176
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-07-04 02:16:52 -0600

    The settings_view Controller and fxml view got created, now the settings buttons in the mainView sends you to the settings view. The settings view has a choice box to be able to select the position in which the app will be first opened

--- src/main/java/com/brnd08/action_recorder/views/main_view/MainViewController.java
+++ src/main/java/com/brnd08/action_recorder/views/main_view/MainViewController.java
@@ -1,12 +1,19 @@
 package com.brnd08.action_recorder.views.main_view;
 
-import com.brnd08.action_recorder.utils.StagePositioner;
-import javafx.event.ActionEvent;
+import com.brnd08.action_recorder.views.settings_view.SettingsViewController;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import static com.brnd08.action_recorder.utils.StagePositioner.addStageDragFunctionality;
 
 public class MainViewController {
 
@@ -14,7 +21,6 @@ public class MainViewController {
     Button minimizeBttn, closeBttn;
     @FXML
     Button playBttn, recordBttn, settingsBttn;
-    private StagePositioner stagePositioner;
 
     @FXML
     public void minimizeStage(Event event) {
@@ -22,16 +28,13 @@ public class MainViewController {
     }
 
     @FXML
-    public void closeStage(ActionEvent event) {
+    public void closeStage(Event event) {
         ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
     }
 
     public void showNewRecordScene(Event event) {
         Stage previousStage = ((Stage) (((Button) event.getSource()).getScene().getWindow()));
 
-        if (stagePositioner == null) {
-            stagePositioner = new StagePositioner(previousStage);
-        }
 
         double stageWidth = previousStage.getWidth();
         double stageHeight = previousStage.getHeight();
@@ -41,27 +44,49 @@ public class MainViewController {
         System.out.println("You should be able to see New Record scene now");
     }
 
-    public void showSettingsScene(Event event) {
+    public void showSettingsScene(Event event) throws IOException {
         Stage previousStage = ((Stage) (((Button) event.getSource()).getScene().getWindow()));
 
-        if (stagePositioner == null) {
-            stagePositioner = new StagePositioner(previousStage);
+        double sceneWidth = previousStage.getScene().getWidth();
+        double sceneHeight = previousStage.getScene().getHeight();
+
+        Image programIcon = previousStage.getIcons().get(0);
+        previousStage.close();
+        System.out.println("You should be able to Settings scene now");
+
+        // Load Fxml view
+        FXMLLoader fxmlLoader;
+
+        try {
+            fxmlLoader =
+                    new FXMLLoader(
+                            Objects.requireNonNull(
+                                    SettingsViewController.class.getResource("settingsView.fxml")
+                            )
+                    );
+        } catch (NullPointerException e) {
+            System.out.println("No se encontro el archivo settingsView.fxml");
+            throw e;
         }
 
-        double stageWidth = previousStage.getWidth();
-        double stageHeight = previousStage.getHeight();
-        double xStageCoordinate = previousStage.getX();
-        double yStageCoordinate = previousStage.getY();
-        Image programIcon = previousStage.getIcons().get(0);
-        System.out.println("You should be able to Settings scene now");
+        Parent root = fxmlLoader.load();
+        // Add the fxml view to a new scene with the previous scene width and height
+        Scene newScene = new Scene(root, sceneWidth, sceneHeight);
+
+
+        // Adds the new scene content to the application's Stage
+        previousStage.setScene(newScene);
+        // Modify the stage title
+        previousStage.setTitle("Settings || Action Recorder");
+
+        addStageDragFunctionality(previousStage, newScene);
+        // display the stage on the screen
+        previousStage.show();
     }
 
     public void showPlayRecordScene(Event event) {
         Stage previousStage = ((Stage) (((Button) event.getSource()).getScene().getWindow()));
 
-        if (stagePositioner == null) {
-            stagePositioner = new StagePositioner(previousStage);
-        }
 
         double stageWidth = previousStage.getWidth();
         double stageHeight = previousStage.getHeight();
@@ -74,9 +99,6 @@ public class MainViewController {
     public void enableCoordinatesMode(Event event) {
         Stage previousStage = ((Stage) (((Button) event.getSource()).getScene().getWindow()));
 
-        if (stagePositioner == null) {
-            stagePositioner = new StagePositioner(previousStage);
-        }
 
         double stageWidth = previousStage.getWidth();
         double stageHeight = previousStage.getHeight();
---
+++ src/main/java/com/brnd08/action_recorder/views/settings_view/SettingsViewController.java
@@ -0,0 +1,69 @@
+package com.brnd08.action_recorder.views.settings_view;
+
+import com.brnd08.action_recorder.utils.StagePosition;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class SettingsViewController implements Initializable {
+
+    StagePosition actualWindowPosition;
+
+    @FXML
+    Button minimizeBttn, closeBttn;
+
+    @FXML
+    ChoiceBox<String> positionChoice;
+
+    private StagePosition getSavedPositionFromDatabase() {
+        // here you should retrieve the Stage Position from the database
+        return StagePosition.LOWER_RIGHT_CORNER;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // adds the list containing the toShowStrings from the StagePosition constants
+        positionChoice.getItems().addAll(
+                Arrays.stream(StagePosition.values()).map(StagePosition::getToShowString).toList()
+        );
+
+        // gets the initial Stage Position
+        actualWindowPosition = getSavedPositionFromDatabase();
+        // makes the option corresponding to the actual Position selected in the choice box
+        positionChoice.getSelectionModel().select(actualWindowPosition.getToShowString());
+
+        positionChoice.setOnAction(event -> {
+            String choice = positionChoice.getValue();
+            //obtains the enum constant based on his toShowString value and the choice value.
+            actualWindowPosition =
+                    Arrays.stream(StagePosition.values())
+                            .filter(
+                                    stagePosition -> stagePosition.getToShowString().equals(choice)
+                            ).findFirst().orElseThrow();
+            System.out.println(actualWindowPosition);
+        });
+    }
+
+    @FXML
+    public void minimizeStage(Event event) {
+        ((Stage) (((Button) event.getSource()).getScene().getWindow())).setIconified(true);
+    }
+
+    @FXML
+    public void closeStage(Event event) {
+        ((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
+    }
+
+
+    @FXML
+    public void showMainView(Event event) {
+        System.out.println("Now you should be able to see the main-view scene.");
+    }
+}
