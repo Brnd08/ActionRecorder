20559549d055f6c0ec76b04376630884ef3e9c57
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-07-06 21:37:01 -0600

    The views.utils.CommonViewUtils class got added as a Utility class with methods for views loading/showing and navigation between views. Also modify the ViewEnum class for readability purposes and delete unused Icon views/main_view/appIcon.gif.

---
+++ src/main/java/com/brnd08/action_recorder/views/utils/CommonViewUtils.java
@@ -0,0 +1,88 @@
+package com.brnd08.action_recorder.views.utils;
+
+import com.brnd08.action_recorder.views.settings_view.SettingsViewController;
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
+
+import static com.brnd08.action_recorder.views.utils.StagePositioner.addDragFunctionalityToStage;
+
+public abstract class CommonViewUtils {
+
+
+    private CommonViewUtils(){
+        // to prevent class instantiation
+        throw new UnsupportedOperationException("Utility class can not be instantiated");
+    }
+    
+    public static void openView(Stage stage, ViewEnum nextView) throws IOException {
+        // Load Fxml view
+        FXMLLoader fxmlLoader;
+
+        try {
+            fxmlLoader =
+                    new FXMLLoader(
+                            Objects.requireNonNull(
+                                    ViewEnum.class.getResource(
+                                            nextView.getFxmlPath() // get the fxml file path from View constant
+                                    )
+                            )
+                    );
+        } catch (NullPointerException e) {
+            System.out.println("No se encontro el archivo settingsView.fxml");
+            throw e;
+        }
+
+        Parent root = fxmlLoader.load();
+
+        // Add the fxml view to a new scene with the previous scene width and height
+        Scene newScene = new Scene(root);
+
+
+        // Needed configuration to make the application background transparent
+        newScene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        stage.setResizable(false);
+
+        // Adds taskbar icon
+        stage.getIcons().add(0, nextView.getAppIcon() );
+        // Modify the stage title
+        stage.setTitle(nextView.getStageTitle());
+
+
+        // Adds the new scene to the Stage
+        stage.setScene(newScene);
+
+
+        // makes stage draggable by mouse interaction
+        addDragFunctionalityToStage(stage, newScene);
+
+        // display the stage on the screen
+        stage.show();
+        
+    }
+
+    public static void navigateToView(Event clickEvent, ViewEnum nextView) throws IOException, NullPointerException {
+
+        Stage previousStage =
+                (Stage) ((Button) clickEvent.getSource())
+                        .getScene()
+                        .getWindow();
+
+        previousStage.close();
+
+        openView(new Stage(), nextView);
+
+    }
+}
--- src/main/java/com/brnd08/action_recorder/views/utils/ViewEnum.java
+++ src/main/java/com/brnd08/action_recorder/views/utils/ViewEnum.java
@@ -8,22 +8,29 @@ public enum ViewEnum {
 
     MAIN(
             "Action Recorder || By brdn",
-            "/main_view/mainView.fxml"),
+            "/main_view/mainView.fxml"
+    ),
     SETTINGS(
             "Settings || Action Recorder",
-            "/settings_view/settingsView.fxml");
-
+            "/settings_view/settingsView.fxml"
+    );
 
     private static final String PATH_TO_VIEWS_PACKAGE_FROM_SOURCE_ROOT
             = "/com/brnd08/action_recorder/views";
+
     private static final Image APP_ICON =
             new Image(
                     Objects.requireNonNull(
-                            ViewEnum.class.getResourceAsStream(PATH_TO_VIEWS_PACKAGE_FROM_SOURCE_ROOT + "/images/appIcon.gif")
+                            ViewEnum.class.getResourceAsStream(
+                                    PATH_TO_VIEWS_PACKAGE_FROM_SOURCE_ROOT + "/images/appIcon.gif"
+                            )
                     )
             );
+
     private final String stageTitle;
+
     private final String fxmlPath;
+
     ViewEnum(String stageTitle, String fxmlUrlFromViewsPackage) {
         this.stageTitle = stageTitle;
         this.fxmlPath = PATH_TO_VIEWS_PACKAGE_FROM_SOURCE_ROOT + fxmlUrlFromViewsPackage;
@@ -37,7 +44,7 @@ public enum ViewEnum {
         return this.fxmlPath;
     }
 
-    public Image getAppIcon(){
+    public Image getAppIcon() {
         return APP_ICON;
     }
 
