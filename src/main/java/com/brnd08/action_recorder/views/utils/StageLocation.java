b586d72b6129736f2414d66c69b0713eb350beda
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-07-10 18:26:53 -0600

    Add two methods to the StageLocation Enum. Those methods got added in order to get used by the settings controller and make easier to show and retrieve the toShowStrings and the related constants to each one.

--- src/main/java/com/brnd08/action_recorder/views/utils/StageLocation.java
+++ src/main/java/com/brnd08/action_recorder/views/utils/StageLocation.java
@@ -1,5 +1,8 @@
 package com.brnd08.action_recorder.views.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * This enum contains constants for each available position of the Stages relative to
  * the screen bounds
@@ -11,12 +14,25 @@ public enum StageLocation {
     LOWER_LEFT_CORNER("Esquina inferior izquierda."),
     CENTER("Centro.");
 
-    private StageLocation(String toShow){
+    private final String toShowString;
+
+    private StageLocation(String toShow) {
         this.toShowString = toShow;
     }
 
-    public String getToShowString(){
+    public static StageLocation stageLocationFromToShowString(String toShowString) {
+        return Arrays.stream(StageLocation.values())
+                .filter(stagePosition -> stagePosition.getToShowString().equals(toShowString))
+                .findFirst().orElseThrow();
+    }
+    public static List<String> getToShowStringsList(){
+        return Arrays
+                .stream(StageLocation.values())
+                .map(StageLocation::getToShowString)
+                .toList();
+    }
+
+    public String getToShowString() {
         return this.toShowString;
     }
-    private final String toShowString;
 }
