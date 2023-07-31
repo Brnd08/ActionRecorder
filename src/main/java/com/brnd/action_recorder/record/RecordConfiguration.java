618fd862a03a44cb4657ccaf17f7703106d6e74c
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-07-30 22:17:49 -0600

    Add RecordConfiguration class as a way to hold and set either which inputEvent should be recorded and ignored

---
+++ src/main/java/com/brnd/action_recorder/record/RecordConfiguration.java
@@ -0,0 +1,67 @@
+package com.brnd.action_recorder.record;
+
+public class RecordConfiguration {
+    private boolean recordingKeyboardInteractions;
+    private boolean recordingMouseMotionInteractions;
+
+    public boolean isRecordingKeyboardInteractions() {
+        return recordingKeyboardInteractions;
+    }
+
+    public void setRecordingKeyboardInteractions(boolean recordingKeyboardInteractions) {
+        this.recordingKeyboardInteractions = recordingKeyboardInteractions;
+    }
+
+    public boolean isRecordingMouseMotionInteractions() {
+        return recordingMouseMotionInteractions;
+    }
+
+    public void setRecordingMouseMotionInteractions(boolean recordingMouseMotionInteractions) {
+        this.recordingMouseMotionInteractions = recordingMouseMotionInteractions;
+    }
+
+    public boolean isRecordingMouseClickInteractions() {
+        return recordingMouseClickInteractions;
+    }
+
+    public void setRecordingMouseClickInteractions(boolean recordingMouseClickInteractions) {
+        this.recordingMouseClickInteractions = recordingMouseClickInteractions;
+    }
+
+    public boolean isRecordingMouseWheelInteractions() {
+        return recordingMouseWheelInteractions;
+    }
+
+    public void setRecordingMouseWheelInteractions(boolean recordingMouseWheelInteractions) {
+        this.recordingMouseWheelInteractions = recordingMouseWheelInteractions;
+    }
+
+    private boolean recordingMouseClickInteractions;
+    private boolean recordingMouseWheelInteractions;
+
+    public RecordConfiguration(boolean recordingKeyboardInteractions, boolean recordingMouseMotionInteractions, boolean recordingMouseClickInteractions, boolean recordingMouseWheelInteractions) {
+        this.recordingKeyboardInteractions = recordingKeyboardInteractions;
+        this.recordingMouseMotionInteractions = recordingMouseMotionInteractions;
+        this.recordingMouseClickInteractions = recordingMouseClickInteractions;
+        this.recordingMouseWheelInteractions = recordingMouseWheelInteractions;
+    }
+    public RecordConfiguration(){
+        this.recordingKeyboardInteractions = true;
+        this.recordingMouseMotionInteractions = true;
+        this.recordingMouseClickInteractions = true;
+        this.recordingMouseWheelInteractions = true;
+    }
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("RecordConfiguration{");
+        sb.append("recordKeyboardInteractions=").append(recordingKeyboardInteractions);
+        sb.append(", recordMouseMotionInteractions=").append(recordingMouseMotionInteractions);
+        sb.append(", recordMouseClickInteractions=").append(recordingMouseClickInteractions);
+        sb.append(", recordMouseWheelInteractions=").append(recordingMouseWheelInteractions);
+        sb.append('}');
+        return sb.toString();
+    }
+
+
+
+}
