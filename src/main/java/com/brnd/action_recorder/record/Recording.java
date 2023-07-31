0e136fba5aa6366ffd43f09c2af40cf7e91a6681
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-07-30 22:09:33 -0600

    Replace the interactions List of KeyStrokes with a HashMap of Long and NativeInputEvent in order to be able to store all the captured user-captured interactions with the time they occured. Also remove the LocalDateTime and a long attribute to store the execution time in which a Recording object was instantiated, also implement the Serializable class

--- src/main/java/com/brnd/action_recorder/record/Recording.java
+++ src/main/java/com/brnd/action_recorder/record/Recording.java
@@ -1,12 +1,32 @@
 package com.brnd.action_recorder.record;
 
-import java.time.LocalDateTime;
+import com.github.kwhat.jnativehook.NativeInputEvent;
+
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
-public class Recording {
-    private int id;
-    private LocalDateTime dateTime;
-    private ArrayList<KeyStroke> interactions;
+public class Recording implements Serializable {
+    private final int id;
+    private final LinkedHashMap<Long, NativeInputEvent> interactions;
+    private final long recordingStartTime;
 
+    public Recording() {
+        this.recordingStartTime = System.nanoTime();
+        this.interactions = new LinkedHashMap<>();
+        this.id = 1;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public LinkedHashMap<Long, NativeInputEvent> getInteractions() {
+        return interactions;
+    }
+
+    public long getRecordingStartTime() {
+        return recordingStartTime;
+    }
 
 }
