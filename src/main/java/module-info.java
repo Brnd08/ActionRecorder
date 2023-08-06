a47d6f2cc69a4a462871c76fa8468699309b91c2
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-08-05 22:43:19 -0600

    Add JavaFxTrayIcon RunnableTest class to test the correct dependency functionality

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,13 +9,17 @@ module com.brnd.action_recorder {
     requires org.xerial.sqlitejdbc;
     requires com.github.kwhat.jnativehook;
     requires java.base;
+    requires com.dustinredmond.fxtrayicon;
 
     opens com.brnd.action_recorder.views.main_view to javafx.fxml;
     opens com.brnd.action_recorder.views.settings_view to javafx.fxml;
     opens com.brnd.action_recorder.views.recording_start_view to javafx.fxml;
     opens com.brnd.action_recorder.views.utils to javafx.fxml;
+    opens com.brnd.action_recorder.record.capturing to javafx.fxml;
+
 
     exports com.brnd.action_recorder.views.main_view;
     exports com.brnd.action_recorder.views.settings_view;
     exports com.brnd.action_recorder.views.utils;
+    exports com.brnd.action_recorder.record.capturing;
 }
