add55f3ebb00d258e9f1192d1310f9132fac798e
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-08-12 00:01:14 -0600

    Add main view controller and fxml file ffor the save recording view

---
+++ src/main/java/com/brnd/action_recorder/views/recording_saving_view/RecordingSavingViewController.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2023 Brandon Velazquez & contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.brnd.action_recorder.views.recording_saving_view;
+
+import com.brnd.action_recorder.views.utils.ViewController;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.Event;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author brdn
+ */
+public class RecordingSavingViewController implements Initializable, ViewController{
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+// Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void minimizeStage(Event event) {
+        ViewController.super.minimizeStage(event); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void closeStage(Event event) {
+        ViewController.super.closeStage(event); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
